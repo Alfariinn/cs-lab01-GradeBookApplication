f24df8aa7adfc1dcc032c6ae2b164de11741bb95
mgrochot <115351337+mgrochot@users.noreply.github.com>
2023-03-11 19:21:53 +0100

    1/2
    Added support for Ranked Grading

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            double studentCount = Students.Count;
+            int topStudents = 0;
+
+            if (studentCount < 5)
+            {
+                throw new ArgumentException("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                studentCount = studentCount / 5;
+
+                foreach (Student student in Students)
+                    foreach (double grade in student.Grades)
+                        if (grade > averageGrade)
+                            topStudents++;
+            }
+            double percentage = topStudents / studentCount;
+            if (percentage < 1) return 'A';
+            else if (percentage < 2) return 'B';
+            else if (percentage < 3) return 'C';
+            else if (percentage < 4) return 'D';
+            else return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            double studentCount = Students.Count;
+            if (studentCount < 5) 
+                { Console.WriteLine("Ranked grading requires at least 5 students."); }
+            else 
+                base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            double studentCount = Students.Count;
+            if (studentCount < 5)
+            { Console.WriteLine("Ranked grading requires at least 5 students."); }
+            else
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,28 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
-            Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            if (parts[2] == "standard")
+            {
+                BaseGradeBook standardGradeBook = new StandardGradeBook(name);
+                Console.WriteLine("Created standard gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(standardGradeBook);
+
+            }
+            else if (parts[2] == "ranked")
+            {
+                BaseGradeBook rankedGradeBook = new RankedGradeBook(name);
+                Console.WriteLine("Created ranked gradebook {0}.", name);
+                GradeBookUserInterface.CommandLoop(rankedGradeBook);
+
+            }
+            else Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
+  
         }
 
         public static void LoadCommand(string command)
